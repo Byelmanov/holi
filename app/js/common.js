a3092fae7f00457499d4d9bbd4415ddb429cf423
Byelmanov <yelmanovbohdan@gmail.com>
2020-01-13 19:19:40 +0200

    add scripts for hover in footer and fix question mark in application section

--- app/js/common.js
+++ app/js/common.js
@@ -1,54 +1,60 @@
-// script for hamburger menu
-document.querySelector('#menu_hamburger').addEventListener('click', function () {
-    $('#header_menu').slideToggle(function () {
-        $('#header_menu').animate({ top: '70' }, 500);
-        $('#menu_hamburger').hide();
-        $('#menu_cross').show();
-    });
-});
-document.querySelector('#menu_cross').addEventListener('click', function () {
-    $('#header_menu').animate({ top: '0' }, 500, function () {
-        $('#header_menu').slideToggle(function () {
-            $('#menu_cross').hide();
-            $('#menu_hamburger').show();
-        });
-    });
+'use scrict';
+
+//hover for instagram in footer
+document.getElementById('footerInstagram').addEventListener('mouseover', function () {
+    let imgNode = document.getElementById('footerInstagramImg');
+    let attr = '/img/instagramPink.svg';
+    imgNode.setAttribute('src', attr);
+    let link = document.getElementById('footerInstagramLink');
+    link.style.color = '#EB6FBD';
+    link.style.textDecoration = 'underline';
 
 });
+document.getElementById('footerInstagram').addEventListener('mouseout', function () {
+    let imgNode = document.getElementById('footerInstagramImg');
+    let attr = '/img/instagram.svg';
+    imgNode.setAttribute('src', attr);
+    let link = document.getElementById('footerInstagramLink');
+    link.style.color = '#fff';
+    link.style.textDecoration = 'none';
+});
+//hover for facebook in footer
+document.getElementById('footerFacebook').addEventListener('mouseover', function () {
+    let imgNode = document.getElementById('footerFacebookImg');
+    let attr = '/img/facebookPink.svg';
+    imgNode.setAttribute('src', attr);
+    let link = document.getElementById('footerFacebookLink');
+    link.style.color = '#EB6FBD';
+    link.style.textDecoration = 'underline';
 
-//script for slider
-$('#slider').slick({
-    dots: false,
-    arrows: false,
-    centerMode: false,
-    slidesToShow: 1,
-    infinite: true,
-    draggable: true,
-    autoplay: false,
+});
+document.getElementById('footerFacebook').addEventListener('mouseout', function () {
+    let imgNode = document.getElementById('footerFacebookImg');
+    let attr = '/img/facebook.svg';
+    imgNode.setAttribute('src', attr);
+    let link = document.getElementById('footerFacebookLink');
+    link.style.color = '#fff';
+    link.style.textDecoration = 'none';
+});
+//hover for email in footer
+document.getElementById('footerMail').addEventListener('mouseover', function () {
+    let imgNode = document.getElementById('footerMailImg');
+    let attr = '/img/mailPink.svg';
+    imgNode.setAttribute('src', attr);
+    let link = document.getElementById('footerMailLink');
+    link.style.color = '#EB6FBD';
+    link.style.textDecoration = 'underline';
+
+});
+document.getElementById('footerMail').addEventListener('mouseout', function () {
+    let imgNode = document.getElementById('footerMailImg');
+    let attr = '/img/mail.svg';
+    imgNode.setAttribute('src', attr);
+    let link = document.getElementById('footerMailLink');
+    link.style.color = '#fff';
+    link.style.textDecoration = 'none';
 });
 
-//custom arrows for slider
-$('#sliderNextButton').click(function () {
-    $('#slider').slick('slickNext');
-});
-$("#sliderPrevButton").click(function () {
-    $("#slider").slick("slickPrev");
-});
 
-//custom dots for slider
-$(".slider__dot").click(function () {
-    var scrollTo = $(this).text();
-    $('#slider').slick('slickGoTo', scrollTo);
-});
-// remove active 
-$("#slider").on("beforeChange", function () {
-    var currentPos = $('#slider').slick('slickCurrentSlide');
-    var arrayOfItems = document.getElementsByClassName("slider__dot");
-    arrayOfItems[currentPos].classList.remove("slider__dot--active");
-});
-// add ative
-$('#slider').on('afterChange', function () {
-    var currentPos = $('#slider').slick('slickCurrentSlide');
-    var arrayOfItems = document.getElementsByClassName("slider__dot");
-    arrayOfItems[currentPos].classList.add("slider__dot--active");
-});
+
+
